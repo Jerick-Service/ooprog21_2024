b8a0030acc1f40630a663256124d6ae9df2c6105
Jerick-Service <jerickservice4@gmail.com>
2024-08-29 17:57:02 -0700

    Update TableAndChairs.java

--- chapter1/TableAndChairs.java
+++ chapter1/TableAndChairs.java
@@ -1,1 +1,10 @@
-
+public class TableAndChairs {
+    public static void main(String[]args){
+        System.out.println("X                                     X");
+        System.out.println("X                                     X");
+        System.out.println("X         X X X X X X X X X X         X");
+        System.out.println("X X X X   X                 X   X X X X");
+        System.out.println("X     X   X                 X   X     X");
+        System.out.println("X     X   X                 X   X     X");
+    }
+}
