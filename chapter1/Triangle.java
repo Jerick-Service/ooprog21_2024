2235a9ebdf71dc3a8806840957d0a7bc10b68d1e
Jerick-Service <jerickservice4@gmail.com>
2024-08-29 18:40:21 -0700

    Update Triangle.java

--- chapter1/Triangle.java
+++ chapter1/Triangle.java
@@ -1,9 +1,9 @@
 public class Triangle {
     public static void main(String[] args) {
         
-        System.out.println("   *   ");
-        System.out.println("  ***  ");
-        System.out.println(" ***** ");
-        System.out.println("*******");
+        System.out.println("   I   ");
+        System.out.println("  III  ");
+        System.out.println(" IIIII ");
+        System.out.println("IIIIIII");
     }
 }
