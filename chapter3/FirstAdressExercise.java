93a75bf6d3aec9def8225364803dc4e123456f58
Jerick-Service <jerickservice4@gmail.com>
2024-10-04 09:15:39 +0700

    Create FirstAdressExercise.java

---
+++ chapter3/FirstAdressExercise.java
@@ -0,0 +1,15 @@
+public class FirstAddressExercise {
+
+    public static void main(String[] args) {
+        System.out.println("Huawei Phone");
+
+        displayAddress();
+
+        System.out.println("Company: Huawei Technologies Co., Ltd.");
+    }
+
+    public static void displayAddress() {
+        System.out.println("Shenzhen, Guangdong, China");
+        System.out.println("Huawei Headquarters: Bantian, Longgang District, Shenzhen, 518129");
+    }
+}
