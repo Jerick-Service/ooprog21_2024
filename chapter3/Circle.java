1e15fbb27a562c132b876fedb41c592a8c937afc
Jerick-Service <jerickservice4@gmail.com>
2024-10-04 08:13:22 +0700

    Update Circle.java

--- chapter3/Circle.java
+++ chapter3/Circle.java
@@ -2,8 +2,10 @@ public class TestCirle{
    public static void main(String[] args){
    Circle a = new Circle(0);
    Circle b = new Circle(0);
+   Circle c = new Circle(0);
    a.setRadius(3);
    b.setRadius(20);
+   c.setRadius(23);
    System.out.println("Radius of the first cirlce: "+a.getRadius());
    System.out.println("Diameter of the first cirlce: "+a.getDiameter());
    System.out.println("Area of the first cirlce: "+a.getArea());
@@ -11,6 +13,13 @@ public class TestCirle{
    System.out.println("Radius of the second cirlce: "+b.getRadius());
    System.out.println("Diameter of the second cirlce: "+b.getDiameter());
    System.out.println("Area of the second cirlce: "+b.getArea());
+   System.out.println();
+   System.out.println("Radius of the third cirlce: "+c.getRadius());
+   System.out.println("Diameter of the third cirlce: "+c.getDiameter());
+   System.out.println("Area of the third cirlce: "+c.getArea());
+
+    
+   
    }
 }
 
