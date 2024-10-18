67a260b51abe2957f2a8dfa0ea6346aa4525f440
Jerick-Service <jerickservice4@gmail.com>
2024-10-18 07:40:55 +0700

    BloodData.java

--- chapter4/BloodData.java
+++ chapter4/BloodData.java
@@ -1,44 +1,54 @@
 public class TestBloodData {
     public static void main(String[] args) {
-        BloodData patient = new BloodData(); 
-        patient.setBloodType("B"); 
-        patient.setRhFactor("+");   
-
-        patient.displayBloodInfo(); 
+        BloodData patient1 = new BloodData();
+        patient1.displayBloodInfo();  // should show default "O" and "+"
+        
+        patient1.setBloodType("B");
+        patient1.setRhFactor("-");
+        patient1.displayBloodInfo();  // should show updated "B" and "-"
+        
+        patient1.defaultPatient();
+        patient1.displayBloodInfo();  // should show "A" and "-"
     }
-}                                                                                  
-
-
-
+}
 
 public class BloodData {
-    
-    private String bloodType;
-    private String rhFactor;
+    String bloodType;
+    String rhFactor;
 
-    
     public BloodData() {
-        defaultPatient();
+        this.bloodType = "O";
+        this.rhFactor = "+";
     }
 
-    
-    public void defaultPatient() {
-        this.bloodType = "A"; 
-        this.rhFactor = "-";  
+    public BloodData(String bloodType, String rhFactor) {
+        this.bloodType = bloodType;
+        this.rhFactor = rhFactor;
     }
 
-    
+    public String getBloodType() {
+        return bloodType;
+    }
+
     public void setBloodType(String bloodType) {
         this.bloodType = bloodType;
     }
 
-    
+    public String getRhFactor() {
+        return rhFactor;
+    }
+
     public void setRhFactor(String rhFactor) {
         this.rhFactor = rhFactor;
     }
 
-    
+    public void defaultPatient() {
+        this.bloodType = "A";
+        this.rhFactor = "-";
+    }
+
     public void displayBloodInfo() {
-        System.out.printf("Blood Type: %s, Rh Factor: %s%n", bloodType, rhFactor);
+        System.out.println("Your blood type is: " + bloodType + rhFactor);
+        
     }
 }
