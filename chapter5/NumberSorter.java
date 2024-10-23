abf6da3cb3ef81a91a17daf388324c6ff98b30f0
Jerick-Service <jerickservice4@gmail.com>
2024-10-23 18:13:05 +0800

    Create NumberSorter.java

---
+++ chapter5/NumberSorter.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class NumberSorter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+
+        System.out.print("Enter the third number: ");
+        int num3 = scanner.nextInt();
+
+        
+        if (num1 <= num2 && num2 <= num3) {
+            
+            System.out.println("Numbers in ascending order: " + num1 + " " + num2 + " " + num3);
+        } else if (num1 >= num2 && num2 >= num3) {
+            
+            System.out.println("Numbers in descending order: " + num1 + " " + num2 + " " + num3);
+        } else {
+           
+            int temp;
+            if (num1 > num2) {
+                temp = num1;
+                num1 = num2;
+                num2 = temp;
+            }
+            if (num2 > num3) {
+                temp = num2;
+                num2 = num3;
+                num3 = temp;
+            }
+            if (num1 > num2) {
+                temp = num1;
+                num1 = num2;
+                num2 = temp;
+            }
+            System.out.println("Numbers in ascending order: " + num1 + " " + num2 + " " + num3);
+        }
+    }
+}
